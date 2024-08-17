2cb5dc40b9372fdb9d76fd118ae1adf3a15c0698
muntakimislam <m.i.idrak07@gmail.com>
2024-08-17 12:37:25 +0600

    Init done

---
+++ src/main/java/com/breezebuy/userservice/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.breezebuy.userservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+}
